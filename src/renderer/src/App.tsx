78c9938a86b5562a27047f018245e6cb8beacfdf
Gustavobvs1 <gustavobvs06@gmail.com>
2024-04-14 23:49:42 -0300

    update several functionalities

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -2,33 +2,101 @@ import { useEffect, useState } from "react";
 
 export function App() {
   const [linesCount, setLinesCount] = useState<number[]>([]);
+  const [text, setText] = useState("");
 
   useEffect(() => {
     if (linesCount.length < 1) setLinesCount([1]);
   });
 
-  function handleInput(event) {
-    let lines = 1;
-    for (let i = 0; i < event.target.value.length; i++) {
-      if (event.target.value[i] === "\n") {
-        lines++;
-      }
+  function getMachingChars(char) {
+    switch (char) {
+      case "{":
+        return "}";
+      case "[":
+        return "]";
+      case "(":
+        return ")";
+      case "'":
+      case '"':
+      case "`":
+        return char;
+      default:
+        return "";
     }
-    const array: number[] = [];
-    for (let i = 1; i <= lines; i++) {
-      array.push(i);
-    }
-    setLinesCount(array);
   }
 
-  function handleBeforeInput(event) {
-    const data = event.target.value;
-    if (event.data === "\n") {
-      const spaces = data.substring(
-        data.substring(0, event.target.selectionStart).lastIndexOf("\n")
+  function handleInput(event) {
+    setText(event.target.value);
+    const lines = event.target.value.split("\n").length;
+
+    setLinesCount(Array.from({ length: lines }, (_, index) => index + 1));
+
+    switch (event.nativeEvent.data) {
+      case "{":
+        setText((prevText) => {
+          return prevText.concat("}");
+        });
+        break;
+      case "[":
+        setText((prevText) => {
+          return prevText.concat("]");
+        });
+        break;
+      case "(":
+        setText((prevText) => {
+          return prevText.concat(")");
+        });
+        break;
+      case "'":
+      case '"':
+      case "`":
+        setText((prevText) => {
+          return prevText.concat(event.nativeEvent.data);
+        });
+        break;
+      default:
+        "";
+        break;
+    }
+  }
+
+  function handleKeyDown(event) {
+    const start = event.target.selectionStart;
+    const end = event.target.selectionEnd;
+    const possibilitiesChars = ["(", "'", '"', "`", "[", "{"];
+    if (event.key === "Enter") {
+      event.preventDefault();
+      const spaces = text
+        .substring(text.lastIndexOf("\n", start) + 1, start)
+        .match(/^\s*/);
+
+      setText(
+        (prevText) =>
+          prevText.slice(0, start) + "\n" + spaces + prevText.slice(end)
       );
-      // .substring(data.lastIndexOf("\n"), data.lastIndexOf(" "));
-      console.log(spaces);
+
+      setLinesCount((prevLinesCount) => [
+        ...prevLinesCount,
+        prevLinesCount.length + 1,
+      ]);
+    } else if (event.key === "Tab") {
+      event.preventDefault();
+      setText(
+        (prevText) => prevText.slice(0, start) + "\t" + prevText.slice(end)
+      );
+    } else if (
+      start !== end &&
+      possibilitiesChars.some((element) => element === event.key)
+    ) {
+      console.log(start, end);
+      setText(
+        (prevText) =>
+          prevText.slice(0, start) +
+          event.key +
+          event.target.value +
+          getMachingChars(event.key) +
+          prevText.slice(end)
+      );
     }
   }
 
@@ -42,7 +110,8 @@ export function App() {
       <textarea
         className="resize-none min-h-full w-full bg-zinc-600 outline-none block font-mono text-white/80 p-2 wh whitespace-nowrap"
         onInput={handleInput}
-        onBeforeInput={handleBeforeInput}
+        onKeyDown={handleKeyDown}
+        value={text}
       />
     </div>
   );
